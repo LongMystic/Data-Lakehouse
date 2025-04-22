e21d20dbac4ee3951403dedc2b81bdf8708521f6
long.vk <long.vk@kiotviet.com>
2025-04-22 18:13:37 +0700

    trying add monitoring

--- services/airflow/dags/utils/notify_telegram.py
+++ services/airflow/dags/utils/notify_telegram.py
@@ -1,5 +1,5 @@
 import requests
-from credentials import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
+from .credentials import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
 
 def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
