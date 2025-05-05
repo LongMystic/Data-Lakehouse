f4b400c97513cf2d593ad17cbe25c1f9342a3099
long.vk <vukhaclong03@gmail.com>
2025-05-05 23:37:49 +0700

    new commit

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-s = "a INT, b INT, c INT,"
-s = s.strip(",\n")
+import os
+import pandas as pd
 
-print(s)
+for file in os.listdir('./data'):
+    df = pd.read_csv(os.path.join('./data', file), nrows=10)
+    print(f"{file}")
+    print(len(df))
+    print(df.columns)
+    print()
+    print()
