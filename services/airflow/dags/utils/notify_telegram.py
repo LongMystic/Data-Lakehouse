33e5e689eaeaffd8a604d9d51c849f32f6137c84
long.vk <vukhaclong03@gmail.com>
2025-06-22 23:33:54 +0700

    replace telegram api to proxy api

--- services/airflow/dags/utils/notify_telegram.py
+++ services/airflow/dags/utils/notify_telegram.py
@@ -2,7 +2,8 @@ import requests
 from .credentials import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
 
 def send_telegram_message(message: str):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    # old_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    url = f"https://late-sunset-71df.vukhaclong03.workers.dev/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
         'chat_id': TELEGRAM_CHAT_ID,
         'text': message,
